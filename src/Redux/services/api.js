9290613c3b87b72dbd03011e890c06b5fefbbb4a
Manoj Kumar das <ephorsys@gmail.com>
2026-09-18 14:47:44 +0530

    add the Live URL

--- src/Redux/services/api.js
+++ src/Redux/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5502/api/v1",
+  baseURL: "https://binayak-industries-backend.onrender.com/api/v1",
   withCredentials: true,
 });
 
